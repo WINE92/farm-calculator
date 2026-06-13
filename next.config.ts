9978bb28186007a3502d793c6ab7b7ad573cf0bf
WINE92 <908966540@qq.com>
2026-06-14 04:55:02 +0800

    优化项目结构，删除未使用代码。

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
-// next.config.ts
 import type { NextConfig } from 'next'
-import withPWA from 'next-pwa'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 }
 
-// 只在生产构建时启用 PWA，开发时完全禁用
-const config = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', // 关键：开发模式禁用
-})(nextConfig)
-
-export default config
+export default nextConfig
